a41e0e497ff720401ed9c874c19b8d449a2574cf
Murilo <murilocarlosr@gmail.com>
2024-02-19 20:43:45 -0300

    commit

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,28 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("User ID=sa;Password=admin;Server=localhost;Database=ExoApi; + Trusted_Connection=True;");
+
+            }
+        }
+        public DbSet<Projeto> Projetos {get; set;}
+    }
+}
